c66a128a2d5fcba1fa333eac84593763775fdd11
Meenakshi-R <meenakshir1135@gmail.com>
2026-06-17 11:07:30 +0530

    Add: n-th-tribonacci-number - submission-6

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-6.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int tribonacci(int n) {
+        int t1=0, t2=1, t3=1;
+        if (n == 0) {
+            return 0;
+        }
+
+        for (int i=3; i<=n; i++) {
+            int result = t1+t2+t3;
+            t1 = t2;
+            t2 = t3;
+            t3 = result;
+        }
+        return t3;
+    }
+}
