b7bf33f22519dd748713854705629bdb2f69741c
Meenakshi-R <meenakshir1135@gmail.com>
2026-07-03 10:17:26 +0530

    Add: valid-tree - submission-13

---
+++ Data Structures & Algorithms/valid-tree/submission-13.java
@@ -0,0 +1,53 @@
+class DSU {
+    int[] parent;
+    int[] size;
+
+    public DSU(int n) {
+        parent = new int[n+1];
+        size = new int[n+1];
+        for (int i=0; i<n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int findParent(int node) {
+        if (parent[node] != node) {
+            parent[node] = findParent(parent[node]);
+        }
+        return parent[node];
+    }
+
+    public boolean union(int u, int v) {
+        int pu = findParent(u);
+        int pv = findParent(v);
+        if (pu == pv) {
+            return false;
+        }
+        if (size[pu] < size[pv]) {
+            size[pv] += size[pu];
+            parent[pv] = pu;
+        }
+        else {
+            size[pu] += size[pv];
+            parent[pu] = pv;
+        }
+        return true;
+    }
+}
+    
+    
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        if (edges.length != n-1) {
+            return false;
+        }
+        DSU dsu = new DSU(n);
+        for (int[] edge : edges) {
+            if(!dsu.union(edge[0], edge[1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
